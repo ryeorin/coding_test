399fcaa42e78a579849cfb4aba20ad7cbb6887ca
ryeorin <130039095+ryeorin@users.noreply.github.com>
2025-11-05 13:39:52 +0900

    [level 2] Title: H-Index, Time: 0.19 ms, Memory: 9.29 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -0,0 +1,7 @@
+def solution(citations):
+    answer = 0
+    citations=sorted(citations,reverse=True)
+    for i in range(len(citations)):
+        if citations[i]<=i:
+            return i
+    return len(citations)
